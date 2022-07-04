ef0a8b77ba0691a530984eb9a00ca645152955d1
davidcecelja <46240796+davidcecelja@users.noreply.github.com>
2022-07-04 11:34:43 +0200

    Add project files.

---
+++ web_app/Controllers/FirstController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web_app.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: First
+        public ActionResult Index()
+        {
+            ViewBag.Lokacija = "MEV";
+            return View();
+        }
+        public ActionResult Home()
+        {
+            ViewBag.Ustanova = "Međimursko veleučilište";
+            ViewData["Lokacija"] = "Čakovec";
+            return View();
+        }
+        public ActionResult Treca(string poruka, int broj = 1)
+        {
+            ViewBag.Poruka = poruka;
+            ViewBag.Broj = broj;
+            return View();
+        }
+        public ActionResult Student()
+        {
+            ViewBag.Ime = "David";
+            ViewBag.Prezime = "Cecelja";
+            ViewBag.GodRodjenja = 1983;
+            return View();
+        }
+    }
+}
