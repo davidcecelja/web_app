19d89da81c3e354fed527700f85680ffd7ec1c7d
davidcecelja <46240796+davidcecelja@users.noreply.github.com>
2022-07-20 18:36:12 +0200

    Autekntifikacija

--- web_app/Models/BazaDbContext.cs
+++ web_app/Models/BazaDbContext.cs
@@ -12,5 +12,7 @@ namespace web_app.Models
     {
         public DbSet<Student> PopisStudenata { get; set; }
         public DbSet<Smjer> PopisSmjerova { get; set; }
+        public DbSet<Korisnik> PopisKorisnika { get; set; }
+        public DbSet<Ovlast> PopisOvlasti { get; set; }
     }
 }
