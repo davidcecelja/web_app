1cb77609bb46e6749f08acbdf662512eb183b095
davidcecelja <46240796+davidcecelja@users.noreply.github.com>
2022-07-21 00:39:52 +0200

    Korisnici add

---
+++ web_app/Controllers/KorisniciController.cs
@@ -0,0 +1,20 @@
+﻿using web_app.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web_app.Controllers
+{
+    public class KorisniciController : Controller
+    {
+        BazaDbContext bazaPodataka = new BazaDbContext();
+        // GET: Korisnici
+        public ActionResult Index()
+        {
+            var listaKorisnika = bazaPodataka.PopisKorisnika.OrderBy(x => x.SifraOvlasti).ThenBy(x => x.Prezime).ToList();
+            return View(listaKorisnika);
+        }
+    }
+}
