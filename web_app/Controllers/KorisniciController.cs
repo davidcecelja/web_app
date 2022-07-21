b2dd2fb9a994d9fcf4f84fac8ab7b4ecaa426022
davidcecelja <46240796+davidcecelja@users.noreply.github.com>
2022-07-22 00:40:15 +0200

    Authent finnish add

--- web_app/Controllers/KorisniciController.cs
+++ web_app/Controllers/KorisniciController.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using web_app.Misc;
+using System.Web.Script.Serialization;
+using System.Web.Security;
 
 namespace web_app.Controllers
 {
@@ -15,6 +18,59 @@ namespace web_app.Controllers
         {
             var listaKorisnika = bazaPodataka.PopisKorisnika.OrderBy(x => x.SifraOvlasti).ThenBy(x => x.Prezime).ToList();
             return View(listaKorisnika);
+        }
+        public ActionResult Prijava(string returnUrl)
+        {
+            KorisnikPrijava model = new KorisnikPrijava();
+            ViewBag.ReturnUrl = returnUrl;
+            return View(model);
+        }
+        public ActionResult Prijava(KorisnikPrijava model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var korisnikBaza = bazaPodataka.PopisKorisnika.FirstOrDefault(x => x.KorisnickoIme == model.KorisnickoIme);
+                if (korisnikBaza != null)
+                {
+                    var passwordOK = korisnikBaza.Lozinka == Misc.PasswordHelper.IzracunajHash(model.Lozinka);
+                    if (passwordOK)
+                    {
+                        LogiraniKorisnik prijavljeniKorisnik = new LogiraniKorisnik(korisnikBaza);
+                        LogiraniKorisnikSerializeModel serializeModel = new LogiraniKorisnikSerializeModel();
+                        serializeModel.CopyFromUser(prijavljeniKorisnik);
+                        JavaScriptSerializer serializer = new JavaScriptSerializer();
+                        string korisnickiPodaci = serializer.Serialize(serializeModel);
+
+                        FormsAuthenticationTicket authTicket = new FormsAuthenticationTicket(
+                            1,
+                            prijavljeniKorisnik.Identity.Name,
+                            DateTime.Now,
+                            DateTime.Now.AddDays(1),
+                            false,
+                            korisnickiPodaci
+                            );
+
+                        string ticketEncrypted = FormsAuthentication.Encrypt(authTicket);
+
+                        HttpCookie cookie = new HttpCookie(FormsAuthentication.FormsCookieName, ticketEncrypted);
+                        Response.Cookies.Add(cookie);
+
+                        if (!String.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+                        {
+                            return Redirect(returnUrl);
+                        }
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Neispravno korisničko ime ili lozinka");
+            return View();
+        }
+        public ActionResult Odjava()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
